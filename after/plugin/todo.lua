8d86cebe7a9e44be298fef21111284b3e30c6d0b
menzicode <mhmenzi@gmail.com>
2024-02-26 17:00:39 -0800

    Added DAP, TROUBLE, and TODO plugins

---
+++ after/plugin/todo.lua
@@ -0,0 +1,14 @@
+-- <leader>td brings up todotrouble
+vim.keymap.set("n", "<leader>td", "<cmd>TodoTrouble<cr>",
+  {silent = true, noremap = true}
+)
+vim.keymap.set("n", "<leader>tD", "<cmd>TodoTelescope<cr>",
+  {silent = true, noremap = true}
+)
+
+-- TODO: 
+-- FIX:
+-- HACK:
+-- PERF:
+-- NOTE:
+-- WARNING:
